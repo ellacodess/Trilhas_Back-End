8ace30535bf37868c5d044d76119cab7ec3327f9
Manoella Marques <77moneymi@gmail.com>
2025-02-23 17:03:53 -0300

    Add files via upload

---
+++ Desafios/Desafio01/item02.js
@@ -0,0 +1,9 @@
+/*
+2. Crie duas variáveis: uma chamada idade e outra chamada altura. Atribua a idade o valor 25 e 
+a altura o valor 1.75. Exiba ambos os valores.
+*/
+
+let idade = 25;
+let altura = 1.75;
+
+alert(`Idade: ${idade}\nAltura: ${altura}`);
