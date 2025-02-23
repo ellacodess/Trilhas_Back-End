885ad38265eb05ebce7b7c3c5182541c46753e66
Manoella Marques <77moneymi@gmail.com>
2025-02-23 17:27:27 -0300

    Delete Desafio 01 directory

--- Desafio 01/item08.js
+++
@@ -1,9 +0,0 @@
-/*
-8. Crie uma variável chamada nome e uma variável chamada idade. Exiba a 
-mensagem "Olá, meu nome é [nome] e eu tenho [idade] anos", utilizando concatenação.
-*/
-
-let nome = 'Manu';
-let idade = 21;
-
-alert(`Olá, meu nome é ${nome} e eu tenho ${idade} anos`);
